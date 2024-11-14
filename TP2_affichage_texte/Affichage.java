9ed9ec8a23cd0693485ca81cd96e85fe600049ab
Côme Regnier <127310202+ComeRegnier@users.noreply.github.com>
2024-11-14 18:39:19 +0100

    commit initial TP2

---
+++ TP2_affichage_texte/Affichage.java
@@ -0,0 +1,15 @@
+class Affichage extends Thread
+{ 
+    String texte;
+    public Affichage (String txt){texte = txt;}
+    public void run()
+    { 
+        for(int i=0; i<texte.length(); i++)
+        { 
+            System.out.print(texte.charAt(i));
+
+        try {sleep(100);} catch (InterruptedException e){};
+        
+        }
+    }
+}
